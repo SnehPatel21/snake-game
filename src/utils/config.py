4f8fc5ff0865aa9d01e54de01398d5877d5541b0
Sneh Patel <supatel5678.90@gmail.com>
2025-01-29 21:15:29 +0530

    Add utility modules for constants and configuration

---
+++ src/utils/config.py
@@ -0,0 +1,21 @@
+import json
+
+class Config:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.config_data = self.load_config()
+
+    def load_config(self):
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
+
+    def get(self, key, default=None):
+        return self.config_data.get(key, default)
+
+    def set(self, key, value):
+        self.config_data[key] = value
+        self.save_config()
+
+    def save_config(self):
+        with open(self.config_file, 'w') as file:
+            json.dump(self.config_data, file, indent=4)
---
+++ src/utils/constants.py
@@ -0,0 +1,3 @@
+# Game constants
+GAME_SPEED = 10  # Speed of the game
+GAME_SIZE = 20   # Size of the game grid
